1e5e96653a0a65086e2e3f2a93a6cd3dc5e670b6
21f1003692 <21f1003692@student.onlinedegree.iitm.ac.in>
2022-07-24 22:20:38 +0530

    README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Greatest Among Three Numbers
+e.g
+Input -
+1
+2
+3
+Output -
+3
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"21f1003692@student.onlinedegree.iitm.ac.in\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
